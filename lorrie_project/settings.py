ee8df50394803007bede203647cfb8cc0f7529d5
Philip Obiri <philipobiri3.1@gmail.com>
2023-07-20 11:16:31 +0000

    Initial Commit

--- lorrie_project/settings.py
+++ lorrie_project/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(env("DEBUG"))
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
