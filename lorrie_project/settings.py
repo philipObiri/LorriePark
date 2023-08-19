857baa8b26c4de4ddfb29d2c57d2b582ad888a6a
Philip Obiri <philipobiri3.1@gmail.com>
2023-08-19 22:12:25 +0000

    Initial Commit

--- lorrie_project/settings.py
+++ lorrie_project/settings.py
@@ -92,13 +92,16 @@ WSGI_APPLICATION = "lorrie_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# Local Database Configuration
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+
+
+if not DEBUG:
+    DATABASES = {'default': env('DATABASE_URL')}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }}
 
 
 
